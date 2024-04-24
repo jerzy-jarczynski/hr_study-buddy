91a4629e87eb40d3cad7975167d11ee4b67f06ec
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2024-04-24 23:50:16 +0200

    Husky and lint-staged pre-commit test

--- src/views/App.js
+++ src/views/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 console.log('hello');
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
